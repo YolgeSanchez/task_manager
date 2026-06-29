1abb5acebfe5c41859b5e86e1b9d903ef2afdc78
yolgesanchez <imjorgesanchez@gmail.com>
2026-06-29 15:48:20 -0400

    feat: implement PrismaTaskRepository for task management operations

---
+++ src/infraestructure/repositories/PrismaTaskRepository.ts
@@ -0,0 +1,59 @@
+import { Task } from "../../domain/entities/Task.js";
+import type { TaskRepository } from "../../domain/repositories/TaskRepository.js";
+import type { ID, SuccessMessage } from "../../domain/types/types.js";
+import type { TaskModel } from "../../generated/prisma/models.js";
+import { prisma } from "../libs/prisma.js";
+
+export class PrismaTaskRepository implements TaskRepository {
+  async save(task: Task): Promise<Task> {
+    const { createdAt, ...taskData } = task.toJSON();
+
+    const saved = await prisma.task.create({
+      data: {
+        ...taskData,
+      },
+    });
+
+    return this.toEntity(saved);
+  }
+
+  async update(task: Task): Promise<Task> {
+    const { createdAt, ...taskData } = task.toJSON();
+
+    const updated = await prisma.task.update({
+      where: { id: task.id },
+      data: {
+        ...taskData,
+      },
+    });
+
+    return this.toEntity(updated);
+  }
+
+  async deleteById(id: ID): Promise<SuccessMessage> {
+    await prisma.task.delete({
+      where: { id },
+    });
+
+    return `Task has been deleted successfully.`;
+  }
+
+  async findAll(): Promise<Task[]> {
+    const tasks = await prisma.task.findMany();
+    return tasks.map((task) => this.toEntity(task));
+  }
+
+  async findById(id: ID): Promise<Task | null> {
+    const task = await prisma.task.findUnique({ where: { id } });
+    return task ? this.toEntity(task) : null;
+  }
+
+  private toEntity(task: TaskModel): Task {
+    return new Task(task.id, {
+      name: task.name,
+      description: task.description,
+      status: task.status,
+      deadline: task.deadline,
+    });
+  }
+}
