1850158e7201b51c5c788779ab71fd4615caac4e
yolgesanchez <imjorgesanchez@gmail.com>
2026-07-01 03:38:56 -0400

    feat: implement UserController with CRUD operations for user management

---
+++ src/infraestructure/http/containers/user.index.ts
@@ -0,0 +1,28 @@
+import { CreateUserUseCase } from '../../../application/use-cases/user/CreateUserUseCase.js'
+import { DeleteUserUseCase } from '../../../application/use-cases/user/DeleteUserUseCase.js'
+import { FindAllUsersUseCase } from '../../../application/use-cases/user/FindAllUsersUseCase.js'
+import { FindUserByIdUseCase } from '../../../application/use-cases/user/FindUserByIdUseCase.js'
+import { UpdateUserUseCase } from '../../../application/use-cases/user/UpdateUserUseCase.js'
+import { BcryptPasswordHasher } from '../../libs/BcryptPasswordHasher.js'
+import { PrismaUserRepository } from '../../repositories/PrismaUserRepository.js'
+import { UserController } from '../controllers/UserController.js'
+
+const prismaUserRepository = new PrismaUserRepository()
+const bcrypPasswordHasher = new BcryptPasswordHasher()
+
+const createUserUseCase = new CreateUserUseCase(prismaUserRepository, bcrypPasswordHasher)
+const updateUserUseCase = new UpdateUserUseCase(prismaUserRepository)
+const deleteUserUseCase = new DeleteUserUseCase(prismaUserRepository)
+
+const findAllUsersUseCase = new FindAllUsersUseCase(prismaUserRepository)
+const findUserByIdUseCase = new FindUserByIdUseCase(prismaUserRepository)
+
+const userController = new UserController(
+  createUserUseCase,
+  updateUserUseCase,
+  deleteUserUseCase,
+  findAllUsersUseCase,
+  findUserByIdUseCase,
+)
+
+export { userController }
---
+++ src/infraestructure/http/controllers/UserController.ts
@@ -0,0 +1,66 @@
+import type { NextFunction, Request, Response } from 'express'
+import type { CreateUserUseCase } from '../../../application/use-cases/user/CreateUserUseCase.js'
+import type { DeleteUserUseCase } from '../../../application/use-cases/user/DeleteUserUseCase.js'
+import type { FindAllUsersUseCase } from '../../../application/use-cases/user/FindAllUsersUseCase.js'
+import type { FindUserByIdUseCase } from '../../../application/use-cases/user/FindUserByIdUseCase.js'
+import type { UpdateUserUseCase } from '../../../application/use-cases/user/UpdateUserUseCase.js'
+import type { UserInput } from '../../../application/dtos/user.dto.js'
+
+export class UserController {
+  constructor(
+    private readonly createUserUseCase: CreateUserUseCase,
+    private readonly updateUserUseCase: UpdateUserUseCase,
+    private readonly deleteUserUseCase: DeleteUserUseCase,
+
+    private readonly findAllUsersUseCase: FindAllUsersUseCase,
+    private readonly findUserByIdUseCase: FindUserByIdUseCase,
+  ) {}
+
+  createUser = async (req: Request<{}, {}, UserInput>, res: Response, next: NextFunction) => {
+    try {
+      const user = await this.createUserUseCase.execute(req.body)
+      res.status(201).json(user)
+    } catch (err) {
+      next(err)
+    }
+  }
+
+  updateUser = async (req: Request<{ id: string }, {}, UserInput>, res: Response, next: NextFunction) => {
+    try {
+      const { id } = req.params
+      const user = await this.updateUserUseCase.execute(id, req.body)
+      res.status(200).json(user)
+    } catch (err) {
+      next(err)
+    }
+  }
+
+  deleteUser = async (req: Request<{ id: string }, {}, {}>, res: Response, next: NextFunction) => {
+    try {
+      const { id } = req.params
+      const message = await this.deleteUserUseCase.execute(id)
+      res.status(200).json({ message })
+    } catch (err) {
+      next(err)
+    }
+  }
+
+  findAllUsers = async (_req: Request, res: Response, next: NextFunction) => {
+    try {
+      const users = await this.findAllUsersUseCase.execute()
+      res.status(200).json(users)
+    } catch (err) {
+      next(err)
+    }
+  }
+
+  findUserById = async (req: Request<{ id: string }, {}, {}>, res: Response, next: NextFunction) => {
+    try {
+      const { id } = req.params
+      const user = await this.findUserByIdUseCase.execute(id)
+      res.status(200).json(user)
+    } catch (err) {
+      next(err)
+    }
+  }
+}
