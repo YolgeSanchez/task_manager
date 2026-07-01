7d89748a8d2b86293a3c0cf21073d6d8f1fb6b22
yolgesanchez <imjorgesanchez@gmail.com>
2026-07-01 17:02:53 -0400

    fix: reorder dotenv import and enhance PrismaClient logging configuration

--- src/infraestructure/libs/prisma.ts
+++ src/infraestructure/libs/prisma.ts
@@ -1,11 +1,11 @@
-import 'dotenv/config'
 import { PrismaPg } from '@prisma/adapter-pg'
+import 'dotenv/config'
 import { PrismaClient } from '../../generated/prisma/client.js'
 
 const connectionString = `${process.env.DATABASE_URL}`
 
 const adapter = new PrismaPg({ connectionString })
-const prisma = new PrismaClient({ adapter })
+const prisma = new PrismaClient({ adapter, log: ['query', 'info', 'warn', 'error'] })
 
 console.log('>>> Prisma Client initialized successfully...')
 
