b0d21475ddd711ac8765690f035fcde4005acf2a
yolgesanchez <imjorgesanchez@gmail.com>
2026-07-01 01:04:33 -0400

    feat: create UserRepository interface for user data management

---
+++ src/domain/repositories/UserRepository.ts
@@ -0,0 +1,12 @@
+import type { User } from "../entities/User.js";
+import type { ID, SuccessMessage } from "../types/types.js";
+
+export interface UserRepository {
+  save(user: User): Promise<User>;
+  update(user: User): Promise<User>;
+  deleteById(id: ID): Promise<SuccessMessage>;
+
+  findAll(): Promise<User[]>;
+  findById(id: ID): Promise<User | null>;
+  findByUsername(username: string): Promise<User | null>;
+}
