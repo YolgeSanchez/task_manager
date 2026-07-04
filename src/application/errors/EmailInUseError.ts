4d610561cae18c68034021a83d5f6e9fd3c9a2a0
yolgesanchez <imjorgesanchez@gmail.com>
2026-07-04 12:45:10 -0400

    fix: update error status codes for EmailInUseError and UsernameInUseError to 409

--- src/application/errors/EmailInUseError.ts
+++ src/application/errors/EmailInUseError.ts
@@ -2,6 +2,6 @@ import { AppError } from '../../domain/errors/AppError.js'
 
 export class EmailInUseError extends AppError {
   constructor() {
-    super(400, 'The email is already in use')
+    super(409, 'The email is already in use')
   }
 }
--- src/application/errors/UsernameInUseError.ts
+++ src/application/errors/UsernameInUseError.ts
@@ -2,6 +2,6 @@ import { AppError } from '../../domain/errors/AppError.js'
 
 export class UsernameInUseError extends AppError {
   constructor() {
-    super(400, 'The username is already in use')
+    super(409, 'The username is already in use')
   }
 }
