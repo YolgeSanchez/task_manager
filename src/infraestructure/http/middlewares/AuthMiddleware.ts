1223dd0aa9758656b77d21c17924d6eb9a69ef78
yolgesanchez <imjorgesanchez@gmail.com>
2026-07-04 13:09:50 -0400

    feat: implement AuthMiddleware for token validation and user assignment

---
+++ src/infraestructure/http/middlewares/AuthMiddleware.ts
@@ -0,0 +1,26 @@
+import type { NextFunction, Request, Response } from 'express'
+import type { TokenService } from '../../../domain/services/TokenService.js'
+import { InvalidTokenError } from '../errors/InvalidTokenError.js'
+import { NoTokenError } from '../errors/NoTokenError.js'
+
+export const AuthMiddleware =
+  (tokenService: TokenService) => async (req: Request, _res: Response, next: NextFunction) => {
+    const token = req.cookies?.token
+
+    // [ verify token existance]
+    if (!token) {
+      next(new NoTokenError())
+      return
+    }
+
+    // [ verify token ]
+    const payload = await tokenService.verifyToken(token)
+    if (!payload) {
+      next(new InvalidTokenError())
+      return
+    }
+
+    // [ assign ]
+    req.user = payload
+    next()
+  }
