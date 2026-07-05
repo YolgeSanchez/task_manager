838dae36fd13d81975edd2f1e70fc3249b7dff92
yolgesanchez <imjorgesanchez@gmail.com>
2026-07-05 18:00:07 -0400

    feat: integrate PrismaProjectRepository into task use cases and controller for project-based task retrieval

--- src/infraestructure/http/containers/repository.index.ts
+++ src/infraestructure/http/containers/repository.index.ts
@@ -1,5 +1,7 @@
+import { PrismaProjectRepository } from '../../repositories/PrismaProjectRepository.js'
 import { PrismaTaskRepository } from '../../repositories/PrismaTaskRepository.js'
 import { PrismaUserRepository } from '../../repositories/PrismaUserRepository.js'
 
 export const prismaTaskRepository = new PrismaTaskRepository()
 export const prismaUserRepository = new PrismaUserRepository()
+export const prismaProjectRepository = new PrismaProjectRepository()
--- src/infraestructure/http/containers/task.index.ts
+++ src/infraestructure/http/containers/task.index.ts
@@ -1,27 +1,32 @@
 import { CreateTaskUseCase } from '../../../application/use-cases/task/CreateTaskUseCase.js'
 import { DeleteTaskUseCase } from '../../../application/use-cases/task/DeleteTaskUseCase.js'
+import { FindAllTasksByProjectIdUseCase } from '../../../application/use-cases/task/FindAllTasksByProjectIdUseCase.js'
 import { FindAllTasksUseCase } from '../../../application/use-cases/task/FindAllTasksUseCase.js'
 import { FindTaskByIdUseCase } from '../../../application/use-cases/task/FindTaskByIdUseCase.js'
 import { UpdateTaskUseCase } from '../../../application/use-cases/task/UpdateTaskUseCase.js'
 import { TaskController } from '../controllers/TaskController.js'
-import { prismaTaskRepository, prismaUserRepository } from './repository.index.js'
+import { prismaProjectRepository, prismaTaskRepository, prismaUserRepository } from './repository.index.js'
 
 const createTaskUseCase = new CreateTaskUseCase(prismaTaskRepository, prismaUserRepository)
 const updateTaskUseCase = new UpdateTaskUseCase(prismaTaskRepository)
 const deleteTaskUseCase = new DeleteTaskUseCase(prismaTaskRepository)
 
 const findAllTasksUseCase = new FindAllTasksUseCase(prismaTaskRepository)
-// const findAllTasksByProjectIdUseCase = new FindAllTasksByProjectIdUseCase(
-//   prismaTaskRepository,
-//   prismaUserRepository,
-// )
+const findAllTasksByProjectIdUseCase = new FindAllTasksByProjectIdUseCase(
+  prismaTaskRepository,
+  prismaUserRepository,
+  prismaProjectRepository,
+)
 const findByIdUseCase = new FindTaskByIdUseCase(prismaTaskRepository)
 
 const taskController = new TaskController(
   createTaskUseCase,
   updateTaskUseCase,
   deleteTaskUseCase,
+
   findAllTasksUseCase,
+  findAllTasksByProjectIdUseCase,
+
   findByIdUseCase,
 )
 
